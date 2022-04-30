64bcbbf29b934d73a84095b0ad57ffa7b9abac90
Martin Wong <kcwong395@gmail.com>
2022-04-30 10:00:21 -0400

    [ops]: add mq config

---
+++ src/main/java/com/wishes/postmanbe/config/RabbitMqConfig.java
@@ -0,0 +1,51 @@
+package com.wishes.postmanbe.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    public static final String TOPIC_EXCHANGE_NAME = "mailing-exchange";
+
+    public static final String QUEUE_NAME = "mailing-queue";
+
+    public static final String ROUTING_KEY = "notification.mailing";
+
+    @Bean
+    Queue mailingQueue() {
+        return new Queue(QUEUE_NAME, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(TOPIC_EXCHANGE_NAME);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(QUEUE_NAME);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(RabbitMqReceiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+}
